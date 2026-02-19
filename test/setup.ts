389359727ebb3737ec468b86f88e4d744c2b8156
johnkryptochain <johncrypto@protonmail.ch>
2026-02-19 23:34:55 +0100

    Fix ResizeObserver ReferenceError in tests

--- test/setup.ts
+++ test/setup.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom'
+
+// Mock ResizeObserver for all tests
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+global.ResizeObserver = ResizeObserver;
